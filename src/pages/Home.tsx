78002f450610a9f2a0bba4c1d5078bf8d7a0674e
Alexis Alfonso <aalfonso88.dev@gmail.com>
2025-08-06 23:24:21 -0300

    home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import me from '../assets/photos/me.png';
+import '../styles/home.css'; // Assuming you have a CSS file for styles
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <main>
+            <section className="home-container" aria-label="Profile overview">
+                <figure className="home-photo-wrapper">
+                    <img
+                        className="home-photo"
+                        src={me}
+                        alt="Photo of Alexis Alfonso"
+                    />
+                </figure>
+
+                <article className="home-text">
+                    <header className='home-text-header'>
+                        <h1 className="home-name">ALEXIS ALFONSO</h1>
+                        <h2 className="home-title">Systems Analyst</h2>
+                    </header>
+                    <div className="home-description">
+                        <p>
+                            Full-Stack Software Developer crafting scalable, <br /> user-focused applications.
+                        </p>
+                    </div>
+                    <nav className="home-nav">
+                        <Link to="/about">
+                            <button className="home-about-button">DISCOVER</button>
+                        </Link>
+                    </nav>
+                </article>
+            </section>
+        </main>
+    );
+};
+
+export default Home;
