e4443cf2d1011386ffc019d67ed85847b1f478e8
Alexis Alfonso <aalfonso88.dev@gmail.com>
2025-10-19 23:33:20 -0300

    fix responsiveness

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,28 +2,41 @@ import React from 'react';
 import '../styles/about-section.css';
 import me from '../assets/photos/me-preview-wm.png';
 import demo from '../assets/vids/demo.mp4'
+import { IconPlay } from './common/Icons';
 
 const AboutSection = () => {
     return (
         <main className="about-section">
             <div className="about-section-text">
-                <p>
-                    <mark className='hl-text'>In 2010</mark>, I started my journey in software development—first as a student, and five years later as a professional. It has been a challenging and exciting experience, taking me through different stages in my career.
-                    After working in data science and ETL for some years, I became more interested in full-stack development, especially for web platforms. In 2022, I made the switch, and I feel it was the right choice.
-                    Today, I’m excited about what the future can bring.
-                </p>
-                <p>
-                    <mark className='hl-text'>In the last three years</mark>, working on software applications has helped me improve both my logical thinking
-                    and my creativity, especially when designing clean and user-friendly interfaces.
-                    This mix has been my biggest motivation.
-                </p>
-                <p>
-                    I was born and raised in <mark className='hl-text'>Montevideo, Uruguay, in 1988.</mark><br />
-                    With this simple portfolio, I want to give you a brief but clear idea of who I am and how I can help you on your projects. I hope we stay in touch and work together in the future.
-                </p>
-                <p>👉 To the right, you can find a short video presentation about me.</p>
+                <section>
+                    <span>Hello World!</span>
+                    <p>
+                        Welcome to my portfolio! This pretends to bring you a short but complete description about me.
+                        All starts in 2010 —first as a student, and five years later as a professional.
+                        It has been a challenging and exciting experience, taking me through different stages in my career.
+                        After working in data science and ETL for some years, I became more interested in full-stack development,
+                        especially for web platforms.
+                    </p>
+                </section>
+                <section>
+                    <span>What about now...</span>
+                    <p>
+                        Since 2022 I work as a Full-Stack Developer, embarked in the exciting trip of developing useful, robust and user focus web applications.
+                        This role implies a great mix of creativity and logical thinking, being my daily motivation.
+                    </p>
+                </section>
+                <section>
+                    <span>What’s next...</span>
+                    <p>
+                        Click on play button to watch a short description of my next goals...
+                    </p>
+                    <div className='about-section-icon-play'>
+                        <button><IconPlay /></button>
+                    </div>
+                </section>
+
             </div>
-            <div className="about-section-vid">
+            <div className="about-section-vid" style={{ display: 'none' }}>
                 <video controls poster={me}>
                     <source src={demo} type="video/mp4" />
                 </video>
--- src/components/common/Icons.tsx
+++ src/components/common/Icons.tsx
@@ -5,3 +5,9 @@ export function ArrowBack(props: SVGProps<SVGSVGElement>) {
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m9.55 12l7.35 7.35q.375.375.363.875t-.388.875t-.875.375t-.875-.375l-7.7-7.675q-.3-.3-.45-.675t-.15-.75t.15-.75t.45-.675l7.7-7.7q.375-.375.888-.363t.887.388t.375.875t-.375.875z" /></svg>
     )
 }
+
+export function IconPlay(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><defs><mask id="IconifyId199ff596389a37fe70"><g fill="none" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z" /><path fill="#000" stroke="#000" d="M20 24v-6.928l6 3.464L32 24l-6 3.464l-6 3.464z" /></g></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#IconifyId199ff596389a37fe70)" /></svg>
+    )
+}
